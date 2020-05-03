a8abbf732d85699fb9324d4600a217c7f31b7be0
Leonardo Carvalho <leohoc@hotmail.com>
2020-05-03 13:41:39 -0300

    Creating a table to store the word count results

--- terraform/database.tf
+++ terraform/database.tf
@@ -19,6 +19,27 @@ resource "aws_dynamodb_table" "citation-dynamodb-table" {
   }
 }
 
+resource "aws_dynamodb_table" "word-count-dynamodb-table" {
+  name           = "Covid19CitationTitlesWordCount"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "word"
+  range_key      = "count"
+
+  attribute {
+    name = "word"
+    type = "S"
+  }
+
+  attribute {
+    name = "count"
+    type = "N"
+  }
+
+  tags = {
+    Name        = "dynamodb-word-count-table"
+  }
+}
+
 resource "aws_s3_bucket" "spark-dynamodb-example" {
   bucket = "spark-dynamodb-example"
   acl    = "private"
